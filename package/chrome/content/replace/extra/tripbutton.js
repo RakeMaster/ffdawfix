643070fbef876647e118df0a4e5e2d6fcbd19efa
Dmitri Novikov <rakemaster111@gmail.com>
2011-12-22 11:08:51 +0400

    tripbutton for blinking message fixed

--- package/chrome/content/replace/extra/tripbutton.js
+++ package/chrome/content/replace/extra/tripbutton.js
@@ -5,18 +5,14 @@ ru.dclan.ffdawfix.replace.observer.add(
 				&&
 			(url.search("http://darkagesworld.com/vr/places/") == 0)
 		);
-		if( r ) {
-			ru.dclan.ffdawfix.utils.log( "Added for " + url );
-		}
 		return r;
 	}
 	,
 	function(text) {
 		// add new row
-		ru.dclan.ffdawfix.utils.log("replacing!!!");
 		var u8 = ru.dclan.ffdawfix.utils.encode;
-		text = text.replace( /<div id="Head_pnlMail2">\s*/, 
-				'<div id="Head_pnlMail2" style="white-space: nowrap">'
+		text = text.replace( /<div id="Head_pnlMail([12])">\s*/, 
+				'<div id="Head_pnlMail\$1" style="white-space: nowrap">'
 				+ '<a style="font-size: 20pt; color: red; font-weight: bold;" href="/vr/Places/Group.aspx?Create=1">'
 				+ u8("Поход")
 				+ '</a>'
