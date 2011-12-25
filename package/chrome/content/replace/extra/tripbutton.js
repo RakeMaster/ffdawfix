05bb71515b3702bd855cba468bda7d6b8b9d4640
Dmitri Novikov <rakemaster111@gmail.com>
2011-12-25 14:38:28 +0400

    Tribbuton moved
    between message image and question image

--- package/chrome/content/replace/extra/tripbutton.js
+++ package/chrome/content/replace/extra/tripbutton.js
@@ -11,14 +11,16 @@ ru.dclan.ffdawfix.replace.observer.add(
 	function(text) {
 		// add new row
 		var u8 = ru.dclan.ffdawfix.utils.encode;
-		text = text.replace( /<div id="Head_pnlMail([12])">\s*/, 
+		text = text.replace( /<div id="Head_pnlMail([12])">\s*<img([^>]+)>\s*/, 
 				'<div id="Head_pnlMail\$1" style="white-space: nowrap">'
+				+ '<img\$2>'
 				+ '<a style="font-size: 20pt; color: red; font-weight: bold;" href="/vr/Places/Group.aspx?Create=1">'
 				+ u8("Поход")
 				+ '</a>'
 				+ '<span onclick="top.MenuLnk(window, 0);" style="text-decoration: underline; cursor: pointer; font-size: 20pt; color: white; font-weight: bold;">'
 				+ u8("Карта")
-				+ '</span>');
+				+ '</span>'
+				);
 		return text;
 	}
 );
