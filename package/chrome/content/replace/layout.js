c456a132503201e61995b955e52fd063ae7df15c
Dmitri Novikov <rakemaster111@gmail.com>
2012-01-18 00:05:02 +0400

    layout replacer removed (fixed in daw)

--- package/chrome/content/replace/layout.js
+++
@@ -1,9 +0,0 @@
-ru.dclan.ffdawfix.replacers.layout = {
-	check: function(url) {
-		return url.search("http://darkagesworld.com/vr/layout.aspx") == 0;
-	},
-	replace: function(text) {
-		text = text.replace( /frame id="chouter"/, 'frame name="chouter" id="chouter"');
-		return text;
-	}
-};
