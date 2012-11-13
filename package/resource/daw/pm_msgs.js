000359039a8ab20b5152f677880d4f2c69290acd
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-11-13 18:01:48 +0200

    Update package/resource/daw/pm_msgs.js

--- package/resource/daw/pm_msgs.js
+++ package/resource/daw/pm_msgs.js
@@ -6,7 +6,7 @@ ffAddOnLoad(function() {
 	for(i=4;i<tds.length;i=i+3){
 		var name = trim(tds[i].textContent.substring(0,tds[i].textContent.indexOf(':')).replace("От",""));
 		if(name!="" && name!="Письмо гласит" && name.indexOf("Вам выдано")==-1){
-			tds[i].innerHTML=tds[i].textContent.replace(name,'<a target="_blank" href='+createPersLinkWithText(name)+'>'+name+'</a><img alt="" onclick="window.location=showMsgMenu()" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">');
+			tds[i].innerHTML=tds[i].textContent.replace(name,'<a target="_blank" href='+createPersLinkWithText(name)+'>'+name+'</a><img alt="" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">');
 		}
 	}
 		injectTag("img",function(node) {
