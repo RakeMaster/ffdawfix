11172ada7dabcce94962e8c5ee0bce5cf1fc4d5c
RakeMaster <rakemaster111@gmail.com>
2012-11-16 13:45:39 +0400

    fixed top frame logic

--- package/chrome/content/replace/top.js
+++ package/chrome/content/replace/top.js
@@ -1,5 +1,5 @@
 ru.dclan.ffdawfix.replacers.topframe = function( f ) {
 	if( !f.checkLocation("default.aspx") && !f.checkLocation("layout.aspx") ) return;
 	// Try to get parent pointer if avaliable. There could be situatetion when default.aspx inside layout.aspx. redirect required
-	f.addJSText( "window.topFramePointer = window; try { window.topFramePointer = parent.window.topFramePointer; } catch(e) {}" );
+	f.addJSText( "window.topFramePointer = window; try { if(parent.window.topFramePointer) { window.topFramePointer = parent.window.topFramePointer;} } catch(e) {}" );
 }
