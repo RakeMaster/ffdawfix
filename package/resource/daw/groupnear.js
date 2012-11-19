a144de90f1fb6c2b0662868389b109662c9db1eb
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-11-19 20:22:29 +0200

    Fixed bugs with images in trip. Added preferences in tools menu

--- package/resource/daw/groupnear.js
+++ package/resource/daw/groupnear.js
@@ -2,14 +2,14 @@ ffAddOnLoad(function() {
 	var headNode = false;
 	injectTag("tr",function(node) {
 		var n = node.children[0];
-		var txt = trim(n.textContent);
+		var txt = n.textContent.trim()
 		if(txt=="нет"){
 			return "delete";
-
 		} 
-		var cur = node.getElementsByTagName("td")[0];
+		var cur = node.children[0];
+
 		if(cur) {
-			var nick = trim(cur.textContent);
+			var nick = cur.textContent.trim()
 			if(nick == "Глава") {
 				headNode = true;
 				return;
@@ -19,9 +19,16 @@ ffAddOnLoad(function() {
 			if(nick=="нет") {
 				return;
 			}
-			var e = createPersLinkWithText(nick);
-			cur.innerHTML = "";
-			cur.appendChild(e);
+			if(cur.innerHTML.indexOf('clb.gif')>-1) {
+				return;
+			}
+			if(cur.innerHTML.indexOf('gang.gif')>-1) {
+				cur.innerHTML = "<img src='http://darkagesworld.com/vr/images/gang.gif'>";
+			}
+			else {
+				cur.innerHTML="";
+			}
+			cur.appendChild(createPersLinkWithText(nick));
 		}
 	});
 });
