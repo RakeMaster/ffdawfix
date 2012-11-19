3536c6576bc390cd0f0701551cc8a8a5bea01d01
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-11-19 10:57:44 +0200

    Removed empty groups from trip

--- package/resource/daw/groupnear.js
+++ package/resource/daw/groupnear.js
@@ -1,6 +1,12 @@
 ffAddOnLoad(function() {
 	var headNode = false;
 	injectTag("tr",function(node) {
+		var n = node.children[0];
+		var txt = trim(n.textContent);
+		if(txt=="нет"){
+			return "delete";
+
+		} 
 		var cur = node.getElementsByTagName("td")[0];
 		if(cur) {
 			var nick = trim(cur.textContent);
