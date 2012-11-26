189e9a3424b06ccc27cf8ad5c32d75ee42acf6df
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-11-26 21:44:25 +0200

    Update pm_msgs.js

--- package/resource/daw/pm_msgs.js
+++ package/resource/daw/pm_msgs.js
@@ -1,17 +1,14 @@
-ffAddOnLoad(function() {
-	var mlist = document.getElementById('pnlMsgList');
-	if(!mlist) return;
-	var tds = mlist.getElementsByTagName('td');
+function openMessagesWithNick(e){
+	window.location="/vr/Menus/Friends.aspx?__EVENTTARGET=ebtSendMsg&__EVENTARGUMENT="+e.target.previousSibling.textContent;
+}
 
-	for(i=4;i<tds.length;i=i+3){
-		var name = trim(tds[i].textContent.substring(0,tds[i].textContent.indexOf(':')).replace("От",""));
-		if(name!="" && name!="Письмо гласит" && name.indexOf("Вам выдано")==-1){
-			tds[i].innerHTML=tds[i].textContent.replace(name,'<a target="_blank" href='+createPersLinkWithText(name)+'>'+name+'</a><img alt="" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">');
+ffAddOnLoad(function(){
+	injectTag("td",function(node){
+		if(node && node.textContent.indexOf('От')>-1){
+			var name = trim(node.textContent.substring(0,node.textContent.indexOf(':')).replace("От",""));
+			if(name=="") return;
+			if(name.indexOf('Сообщения')>-1) return;
+			node.innerHTML = node.innerHTML.replace(name,'<a target="_blank" href='+createPersLinkWithText(name)+'>'+name+'</a><img alt="" onclick="openMessagesWithNick(event)" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">');
 		}
-	}
-		injectTag("img",function(node) {
-			if(node){
-			node.setAttribute('onclick','window.location="/vr/Menus/Friends.aspx?__EVENTTARGET=ebtSendMsg&__EVENTARGUMENT="+this.previousSibling.textContent');
-			}
 	});
 });
