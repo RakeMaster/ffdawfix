265691e03eef3b06122a398493b3f17fabf95641
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-12-17 15:11:25 +0200

    fixed double replace spaces in info

--- package/chrome/content/replace/info.js
+++ package/chrome/content/replace/info.js
@@ -1,4 +1,4 @@
 ru.dclan.ffdawfix.replacers.info = function( f ) {
 	if( !f.checkLocation( "/vr/common/FighterInfo.aspx" ) ) return;
-	f.addReplace(/<\/strong>/g,'</strong>&nbsp;');
+	f.addReplace(/<\/strong>/g,'</strong >&nbsp;');
 }
