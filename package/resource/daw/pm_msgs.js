8c49d2649985a8fe75d0c6df8c5d75021fadff51
Vampir3 <vampir3@vampir3-desktop.(none)>
2012-12-29 16:47:04 +0200

    Added cellpadding in private messages

--- package/resource/daw/pm_msgs.js
+++ package/resource/daw/pm_msgs.js
@@ -1,14 +1,24 @@
 function openMessagesWithNick(e) {
-	window.location = "/vr/Menus/Friends.aspx?__EVENTTARGET=ebtSendMsg&__EVENTARGUMENT=" + e.target.previousSibling.textContent;
+	window.location = "/vr/Menus/Friends.aspx?__EVENTTARGET=ebtSendMsg&__EVENTARGUMENT=" + e;
 }
 
 ffAddOnLoad(function() {
+	var t = document.getElementById('Table1');
+	if(t) {
+		t.cellPadding = "15px";
+	}
 	injectTag("td",function(node) {
 		if(node && node.textContent.indexOf('От') > -1) {
 			var name = trim(node.textContent.substring(0,node.textContent.indexOf(':')).replace("От",""));
 			if(name == "") return;
 			if(name.indexOf('Сообщения') > -1) return;
-			node.innerHTML = node.innerHTML.replace(name,'<a target="_blank" href=' + createPersLinkWithText(name) + '>' + name + '</a><img alt="" onclick="openMessagesWithNick(event)" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">');
+			node.innerHTML = node.innerHTML.replace(name,'<a target="_blank" href='
+			+ createPersLinkWithText(name) 
+			+ '>' 
+			+ name 
+			+ '</a>'
+			+ '<img alt="" onclick="openMessagesWithNick(this.previousSibling.textContent)" style="cursor:pointer; padding-left:3px;" title="Приватное сообщение" src="resource://ffdawfix/img/reply.gif">'
+			);
 		}
 	});
 });
