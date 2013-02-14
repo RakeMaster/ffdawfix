8f94f0a4c656c2d7a005e2a6ad4374bdf8b9a921
Vampir3 <vampir3@vampir3-desktop.(none)>
2013-02-14 21:05:56 +0200

    Redesigned google search

--- package/resource/daw/extra/search.js
+++ package/resource/daw/extra/search.js
@@ -1,11 +1,40 @@
-ffAddOnLoad(function() {
-	var cx = '000837142119810351096:fkqp7dzwfkw';
-	var gcse = document.createElement('script'); 
-	gcse.type = 'text/javascript'; 
-	gcse.async = true;
- 	gcse.src = "resource://ffdawfix/cse.js";
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(gcse, s);
+function checkAnchor() {
+	url = window.location.href;
+	var anc = url.search("#");
+	if(anc == -1) {
+		return "";
+	}
+ 	else {
+ 		return url.substr(anc + 1);
+	}
+}
 
-	document.getElementById('btSearch').parentNode.innerHTML = '<gcse:search></gcse:search>';
+function injectGoogleSearchLink() {
+	var a = document.getElementById('Menu_lbMainMenu').getElementsByTagName('a')[2];
+	var e = document.createElement('a');
+	e.onclick = function() {
+		if(checkAnchor() == "google") {
+			window.location.reload();
+		}
+	}
+	e.href = "http://smuta.com/Forum/search.aspx#google";
+	e.innerHTML = '\u041f\u043e\u0438\u0441\u043a \u043e\u0442 <span style="font-family: Catull BQ;"><font color="blue">G</font><font color="red">o</font><font color="orange">o</font><font color="blue">g</font><font color="green">l</font><font color="red">e</font></span>' + '<br>';
+	a.parentNode.insertBefore(e, a);
+}
+
+ffAddOnLoad(function() {
+	if(checkAnchor() == "google") {
+		var s = document.createElement('script'); 
+		s.type = 'text/javascript'; 
+		s.async = true;
+ 		s.src = "resource://ffdawfix/cse.js";
+		document.head.appendChild(s);
+		document.getElementsByTagName('title')[0].innerHTML = "\u041f\u043e\u0438\u0441\u043a \u043e\u0442 Google";
+
+		var a = document.getElementById('btSearch');
+		if(a) {
+			a.parentNode.innerHTML = '<gcse:search></gcse:search>';
+		}
+	}
+	injectGoogleSearchLink();
 });
