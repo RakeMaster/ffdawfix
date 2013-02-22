2188f46f6c16f1543ab555f86c4f9d9d66149bfa
RakeMaster <rakemaster111@gmail.com>
2013-02-22 15:25:53 +0400

    Tests fix

--- tests/core/replace.js
+++ tests/core/replace.js
@@ -10,17 +10,17 @@ testReplace: function() {
 	r.addCSS("mycss.css");
 	this.assert( r.needReplace() );
 	this.assertEquals( r.replace("text"), "text");
-	this.assertEquals( r.replace("<head></head>"), '<head><link rel="stylesheet" type="text/css" href="resource://ffdawfix/mycss.css" /></head >');
+	this.assertEquals( r.replace("<head></head>"), '<head ><link rel="stylesheet" type="text/css" href="resource://ffdawfix/mycss.css" /></head>');
 	r.addJS("js1.js");
 	r.addJS("js3.js");
 	r.addJS("js2.js");
-	this.assertEquals( r.replace("<head></head>"), '<head><link rel="stylesheet" type="text/css" href="resource://ffdawfix/mycss.css" /><script type="text/javascript" src="resource://ffdawfix/js1.js"></script><script type="text/javascript" src="resource://ffdawfix/js3.js"></script><script type="text/javascript" src="resource://ffdawfix/js2.js"></script></head >');
+	this.assertEquals( r.replace("<head></head>"), '<head ><link rel="stylesheet" type="text/css" href="resource://ffdawfix/mycss.css" /><script type="text/javascript" src="resource://ffdawfix/js1.js"></script><script type="text/javascript" src="resource://ffdawfix/js3.js"></script><script type="text/javascript" src="resource://ffdawfix/js2.js"></script></head>');
 	
 	r = new ru.dclan.ffdawfix.replace.Replacer();
 	this.assert( !r.needReplace() );
 	r.addJSText("alert('hello!');");
 	this.assert( r.needReplace() );
-	this.assertEquals( r.replace("<head></head>"), '<head><script type="text/javascript"><!--\nalert(\'hello!\');\n--></script></head >');
+	this.assertEquals( r.replace("<head></head>"), '<head ><script type="text/javascript"><!--\nalert(\'hello!\');\n--></script></head>');
 	
 	r = new ru.dclan.ffdawfix.replace.Replacer("TheUrl");
 	this.assert( !r.needReplace() );
@@ -33,7 +33,7 @@ testReplace: function() {
 	r.addJS("js1.js");
 	r.addJS("js3.js");
 	r.addJS("js2.js");
-	this.assertEquals( r.replace("<head></head>"), '<head><script type="text/javascript"><!--\nhi\n--></script><script type="text/javascript" src="resource://ffdawfix/js1.js"></script><script type="text/javascript" src="resource://ffdawfix/js3.js"></script><script type="text/javascript" src="resource://ffdawfix/js2.js"></script></head >');
+	this.assertEquals( r.replace("<head></head>"), '<head ><script type="text/javascript"><!--\nhi\n--></script><script type="text/javascript" src="resource://ffdawfix/js1.js"></script><script type="text/javascript" src="resource://ffdawfix/js3.js"></script><script type="text/javascript" src="resource://ffdawfix/js2.js"></script></head>');
 
 }
 });
