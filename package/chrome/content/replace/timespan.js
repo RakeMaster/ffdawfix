bdbb2412b40dc790953f4abacd8725ec87a6f01c
Vampir3 <vampir3@vampir3-desktop.(none)>
2013-03-07 17:34:19 +0200

    Fixed clock (opening frame in new tab)

--- package/chrome/content/replace/timespan.js
+++ package/chrome/content/replace/timespan.js
@@ -1,4 +1,5 @@
 ru.dclan.ffdawfix.replacers.timespan = function( f ) {
 	if( !f.checkLocation( "/vr/common/clock.js" ) ) return;
 	f.addReplace( /timespan\./, 'document.getElementById("timespan").');
+	f.addReplace('function','if(!getTop().clockCorrection) getTop().clockCorrection = -7236843;\nfunction');
 }
