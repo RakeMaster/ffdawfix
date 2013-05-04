3ddd1f73427cb100ea627d7bc4017197ae81374a
Vampir3 <vetal123456@inbox.lv>
2013-05-04 20:58:44 +0400

    On forum added "Get post url" function

--- package/resource/smuta.js
+++ package/resource/smuta.js
@@ -23,6 +23,24 @@ node.innerHTML = node.innerHTML.replace(/\u0413\u043e\u0441\u0442\u044c(,( )?)?/
 	});
 }
 
+function addUrlIcons() {
+	injectTag("a", function(node) {
+		if(node.href.indexOf('PMEdit.aspx') > -1) {
+			var e = document.createElement('img');
+			e.style.marginLeft = "5px";
+			e.style.cursor = "pointer";
+			e.src = "resource://ffdawfix/img/url.png";
+			e.onclick = function() {
+				var rid = node.href.split('&')[1].split('=')[1];
+				var tid = window.location.search.split('&')[0].split('=')[1];
+				var lnk = "http://smuta.com/Forum/Topic.aspx?TID=" + tid + "&rid=" + rid;
+				prompt('\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u043F\u043E\u0441\u0442:', lnk);
+			}
+			node.parentNode.appendChild(e);
+		}
+	});
+}
+
 function addScrollButtons() {
 	h = window.innerHeight/2;
 
@@ -79,4 +97,5 @@ ffAddOnLoad(function() {
 		addScrollButtons();
 	}
 	combineGuests();
+	addUrlIcons();
 });
