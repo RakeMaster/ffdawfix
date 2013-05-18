4d628a6832640e88fdde9c5d1f80968d0eef13e1
Vampir3 <vetal123456@inbox.lv>
2013-05-18 10:05:40 +0400

    Added title on url buttons.

--- package/resource/smuta.js
+++ package/resource/smuta.js
@@ -29,12 +29,13 @@ function addUrlIcons() {
 			var e = document.createElement('img');
 			e.style.marginLeft = "5px";
 			e.style.cursor = "pointer";
+			e.title = "\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u043F\u043E\u0441\u0442";
 			e.src = "resource://ffdawfix/img/url.png";
 			e.onclick = function() {
 				var rid = node.href.split('&')[1].split('=')[1];
 				var tid = window.location.search.split('&')[0].split('=')[1];
 				var lnk = "http://smuta.com/Forum/Topic.aspx?TID=" + tid + "&rid=" + rid;
-				prompt('\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u043F\u043E\u0441\u0442:', lnk);
+				prompt(this.title, lnk);
 			}
 			node.parentNode.appendChild(e);
 		}
