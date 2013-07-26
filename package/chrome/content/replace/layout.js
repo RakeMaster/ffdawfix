54fe05725ba9121cd9e57a85e009d205aae1c206
Vampir3 <vetal123456@inbox.lv>
2013-07-26 13:01:21 +0300

    Fixed extra skills opening in frame (in separate window before)

--- package/chrome/content/replace/layout.js
+++ package/chrome/content/replace/layout.js
@@ -2,4 +2,5 @@ ru.dclan.ffdawfix.replacers.layout = function( f ) {
 	if( !f.checkLocation("layout.aspx") ) return;
 	f.addReplace( /frameborder="0"/gi, 'frameborder="1"');
 	f.addReplace( '+Cash+', '+Number(String(Cash).replace(",",".")).toFixed(2)+');
+	f.addReplace( 'Target="_blank" href="/vr/Menus/UserSkills.aspx"', 'href="javascript:getTop().MenuLnk(this, 8)"' );
 }
